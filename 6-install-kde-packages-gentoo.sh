5a9e4bd1066abb1813d8d96c0c0dcd44d0fa0f9e
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-11-21 22:49:19 +0000

    Update 6-install-kde-packages-gentoo.sh

--- 6-install-kde-packages-gentoo.sh
+++ 6-install-kde-packages-gentoo.sh
@@ -10,3 +10,10 @@ echo "..........................................................."
 echo ">>> Installing desktop packages"
 emerge microsoft-edge youtube-dl simplescreenrecorder app-editors/vscode telegram-desktop-bin nomacs terminator remmina pavucontrol timeshift firefox-bin plocate qbittorrent libreoffice-bin flameshot brave-bin
 echo "..........................................................."
+
+echo "net-misc/xrdp ~amd64" >> /etc/portage/package.accept_keywords/package.accept_keywords 
+echo "net-misc/xorgxrdp ~amd64" >> /etc/portage/package.accept_keywords/package.accept_keywords
+sed -i 's/EMERGE_DEFAULT_OPTS="${EMERGE_DEFAULT_OPTS} --getbinpkgonly"/EMERGE_DEFAULT_OPTS="${EMERGE_DEFAULT_OPTS} --getbinpkgonly --usepkg-exclude xrdp xorgxrdp"/g' /etc/portage/make.conf
+emerge --ask --verbose xorg xorgxrdp
+
+
