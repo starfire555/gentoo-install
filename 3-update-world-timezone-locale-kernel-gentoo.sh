255f6907d875719245bc23627a047703c44d4ee5
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-11-24 09:29:07 +0000

    Update 3-update-world-timezone-locale-kernel-gentoo.sh

--- 3-update-world-timezone-locale-kernel-gentoo.sh
+++ 3-update-world-timezone-locale-kernel-gentoo.sh
@@ -14,7 +14,7 @@ echo 'VIDEO_CARDS="intel"' >> etc/portage/make.conf
 echo "..........................................................."
 
 echo ">>> Updating @world"
-emerge --verbose --ask --update --deep --newuse @world
+emerge --update --deep --newuse @world
 echo "..........................................................."
 
 echo ">>> Setting timezone"
@@ -52,7 +52,7 @@ echo ""
 echo ">>> Helper information that needs to be edited has been added to /etc/fstab"
 cat /proc/mounts | grep btrfs  >> /etc/fstab
 
-cat << EOF >> /etc/fstab
+cat <<EOF >> /etc/fstab
 UUID=       /boot           ext4            rw,relatime     0 0
 UUID=       /boot/efi       vfat            rw,relatime     0 0
 EOF
