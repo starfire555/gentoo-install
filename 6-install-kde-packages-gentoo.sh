3dc6867a51d24f16a4c1672b86edb4330fa9bb0a
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-11-25 19:00:04 +0000

    Update 6-install-kde-packages-gentoo.sh

--- 6-install-kde-packages-gentoo.sh
+++ 6-install-kde-packages-gentoo.sh
@@ -1,3 +1,15 @@
+echo ">>> Setting package USE flags"
+echo "media-libs/libsndfile minimal" >> /etc/portage/package.use/package.use
+echo ">=dev-libs/libdbusmenu-16.04.0-r2 gtk3" >> /etc/portage/package.use/package.use
+echo "net-misc/remmina rdp ssh spice" >> /etc/portage/package.use/package.use
+echo "..........................................................."
+
+echo ">>> Setting package ACCEPT_KEYWORDS"
+echo "app-editors/vscode ~amd64" >> /etc/portage/package.accept_keywords/package.accept_keywords
+echo "app-backup/timeshift ~amd64" >> /etc/portage/package.accept_keywords/package.accept_keywords
+echo "net-im/telegram-desktop-bin ~amd64" >> /etc/portage/package.accept_keywords/package.accept_keywords
+echo "..........................................................."
+
 echo ">>> Installing KDE"
 eselect profile set 10  #systemd desktop plasma
 emerge kde-plasma/plasma-meta
@@ -5,12 +17,6 @@ emerge --update --deep --newuse @world
 emerge kde-apps/kdecore-meta #lessen the MAKEOPTS value
 systemctl set-default graphical
 systemctl enable sddm
-echo "..........................................................."
-
-echo ">>> Setting package ACCEPT_KEYWORDS"
-echo "app-editors/vscode ~amd64" >> /etc/portage/package.accept_keywords/package.accept_keywords
-echo "app-backup/timeshift ~amd64" >> /etc/portage/package.accept_keywords/package.accept_keywords
-echo "net-im/telegram-desktop-bin ~amd64" >> /etc/portage/package.accept_keywords/package.accept_keywords
 echo "..........................................................."
 
 echo ">>> Installing desktop packages"
