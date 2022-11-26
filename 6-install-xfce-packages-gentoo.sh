5beea170edee309b8b2a3fbb07648e12a194ffc5
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-11-26 23:23:14 +0000

    Update 6-install-xfce-packages-gentoo.sh

--- 6-install-xfce-packages-gentoo.sh
+++ 6-install-xfce-packages-gentoo.sh
@@ -6,6 +6,7 @@ echo ">>> Setting package USE flags"
 echo "media-libs/libsndfile minimal" >> /etc/portage/package.use/package.use
 echo ">=dev-libs/libdbusmenu-16.04.0-r2 gtk3" >> /etc/portage/package.use/package.use
 echo "net-misc/remmina rdp ssh spice vnc" >> /etc/portage/package.use/package.use
+echo "sys-libs/zlib minizip" >> /etc/portage/package.use/package.use #for vlc
 echo "..........................................................."
 
 echo ">>> Installing xfce4"
