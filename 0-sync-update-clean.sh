b7d274ec1963c5eee0abf08e0b5292b3f0c2290d
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-11-26 18:36:21 +0000

    Update 0-sync-update-clean.sh

--- 0-sync-update-clean.sh
+++ 0-sync-update-clean.sh
@@ -1,1 +1,2 @@
-emerge --sync && emerge --verbose --ask --update --deep --newuse @world && emerge --depclean
+#emerge --sync && emerge --verbose --ask --update --deep --newuse @world && emerge --depclean
+#ssh-copy-id  -i /home/x/.ssh/id_rsa.pub x@192.168.1.x
