3acb6c92ad939d2bd97d97ffaaf058fdb216610c
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-11-27 08:15:51 +0000

    Update 6-install-gnome-packages-gentoo.sh

--- 6-install-gnome-packages-gentoo.sh
+++ 6-install-gnome-packages-gentoo.sh
@@ -36,3 +36,17 @@ echo ">>> Installing desktop packages"
 emerge --fetchonly microsoft-edge youtube-dl simplescreenrecorder app-editors/vscode nomacs terminator remmina pavucontrol timeshift firefox-bin plocate qbittorrent libreoffice-bin flameshot brave-bin telegram-desktop vlc
 emerge microsoft-edge youtube-dl simplescreenrecorder app-editors/vscode nomacs terminator remmina pavucontrol timeshift firefox-bin plocate qbittorrent libreoffice-bin flameshot brave-bin telegram-desktop vlc
 echo "..........................................................."
+
+echo ">>> Installing xrdp"
+echo net-misc/xrdp ~amd64 >> /etc/portage/package.accept_keywords/package.accept_keywords
+echo net-misc/xorgxrdp ~amd64 >> /etc/portage/package.accept_keywords/package.accept_keywords
+
+emerge xrdp xorgxrdp
+
+systemctl enable --now xrdp
+
+echo '#!/bin/bash' > /home/x/.xinitrc
+echo 'gnome-session' >> /home/x/.xinitrc
+chmod +x /home/x/.xinitrc
+chown x: /home/x/.xinitrc
+echo "..........................................................."
