cdddff966154194748b2da80875058513d28190f
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-11-27 17:53:17 +0000

    Update 0-sync-update-clean.sh

--- 0-sync-update-clean.sh
+++ 0-sync-update-clean.sh
@@ -1,2 +1,3 @@
 #emerge --sync && emerge --verbose --ask --update --deep --newuse @world && emerge --depclean
 #ssh-copy-id  -i /home/x/.ssh/id_rsa.pub x@192.168.1.x
+#pacman -Sy && pacman-key --init && pacman -S --noconfirm git wget && cd / && git clone https://github.com/starfire555/gentoo-install && cd /gentoo-install && chmod +x *
