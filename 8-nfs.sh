146390485f8ba8ea362b8c0ab68e84333957a725
starfire555 <76488394+starfire555@users.noreply.github.com>
2022-12-21 09:05:39 +0000

    Update 8-nfs.sh

--- 8-nfs.sh
+++ 8-nfs.sh
@@ -37,9 +37,9 @@ mkdir -p /mnt/nfs/gentoo-bb/{hdd1,hdd2}
 mkdir -p /mnt/nfs/gentoo-pro/hdd1
 chown -R x: /mnt/nfs
 
-echo 'gentoo-bb:/hdd1    /mnt/nfs/gentoo-bb/hdd1    nfs    rw,_netdev    0    0' >> /etc/fstab
-echo 'gentoo-bb:/hdd2    /mnt/nfs/gentoo-bb/hdd2    nfs    rw,_netdev    0    0' >> /etc/fstab
-echo 'gentoo-pro:/hdd1   /mnt/nfs/gentoo-pro/hdd1   nfs    rw,_netdev    0    0' >> /etc/fstab
+echo 'gentoo-bb:/hdd1    /mnt/nfs/gentoo-bb/hdd1    nfs    rw,_netdev,noauto,user    0    0' >> /etc/fstab
+echo 'gentoo-bb:/hdd2    /mnt/nfs/gentoo-bb/hdd2    nfs    rw,_netdev,noauto,user    0    0' >> /etc/fstab
+echo 'gentoo-pro:/hdd1   /mnt/nfs/gentoo-pro/hdd1   nfs    rw,_netdev,noauto,user    0    0' >> /etc/fstab
 
 systemctl daemon-reload
 mount /mnt/nfs/gentoo-bb/hdd1
